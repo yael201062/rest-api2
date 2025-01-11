3bb14c660da01533d9b1d455267eaff95afbf8fb
yael201062 <yael201062@gmail.com>
2025-01-11 17:48:59 +0200

    models

---
+++ src/models/user_model.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+export interface IUser {
+  email: string;
+  password: string;
+  _id?: string;
+  refreshToken?: string[];
+}
+
+const userSchema = new mongoose.Schema<IUser>({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  refreshToken: {
+    type: [String],
+    default: [],
+  }
+});
+
+const userModel = mongoose.model<IUser>("Users", userSchema);
+
+export default userModel;
